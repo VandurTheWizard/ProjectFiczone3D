50592cfec1a84d2a9e5f15a9add44b4254cf6950
VandurTheWizard <TheWizardVandur@gmail.com>
2025-04-09 12:45:04 +0200

    Temporizador

---
+++ Assets/Script/Ivan/UI/Temporizador.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Temporadizador : MonoBehaviour
+{
+    public float tiempoRestante = 60f;
+    public TextMeshProUGUI textMesh;
+
+    void Start()
+    {
+        if(textMesh == null){
+            textMesh = GetComponent<TextMeshProUGUI>();
+        }   
+    }
+
+    void Update()
+    {
+        tiempoRestante -= Time.deltaTime;
+        if(tiempoRestante <= 0f){
+            tiempoRestante = 0f;
+            Debug.Log("You lose");
+        }   
+        textMesh.text = (int)tiempoRestante + "";
+    }
+
+}
