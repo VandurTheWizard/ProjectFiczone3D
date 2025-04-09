9ee2ae3b8c162687c442fef9691d9290a2b6f26d
VandurTheWizard <156355058+VandurTheWizard@users.noreply.github.com>
2025-04-09 08:11:07 +0200

    hy

--- Assets/Script/Ivan/Ovejas/WinManager.cs
+++ Assets/Script/Ivan/Ovejas/WinManager.cs
@@ -4,70 +4,31 @@ using System.Collections;
 
 public class WinManager : MonoBehaviour
 {
-    [SerializeField] private Image ovejaImage, pulgarImage, fondoImage;
-    [SerializeField] private RawImage camaraImage;
-    public RenderTexture camaraTextura;
-    public Vector3 firstShowSheepPos;
-    public Vector3 showSheepPos;
-    public Vector3 hideSheepPos;
-    public float widthHand = 300f;
-    public float heightHand = 300f;
+    [Header("Referencias")]    
+    [SerializeField] private Image pulgarImage;
+    [SerializeField] private Image fondoImage;
+
+    [Header("Animaciones")]
+    public Vector3 firstShowHandPos;
+    public Vector3 showHandPos;
+    public Vector3 hideHandPos;
+
+
 
     void Start()
     {
-        camaraTextura = new RenderTexture(Screen.width, Screen.height, 24);
-
-        Camera camara = Camera.main;
-
-        camara.targetTexture = camaraTextura;
+        OcultarContenido();
     }
 
-    private void ShowWinScreen()
+    void Update()
     {
-        ovejaImage.gameObject.SetActive(true);
-        pulgarImage.gameObject.SetActive(true);
-        fondoImage.gameObject.SetActive(true);
-        if(camaraImage != null)
-        {
-            camaraImage.texture = camaraTextura;
-        }
-        else
-        {
-            Debug.LogError("RawImage camaraImage no está asignado en el inspector.");
-        }
+        
+    } 
+
+    private void OcultarContenido(){
+        pulgarImage.gameObject.SetActive(false);
+        fondoImage.gameObject.SetActive(false);
     }
 
-    public void OnWin()
-    {
-        Debug.Log("Win!");
-        ShowWinScreen();
-    }
 
-    IEnumerator AnimateSheep()
-    {
-        ovejaImage.rectTransform.position = firstShowSheepPos;
-        float time = 0;
-        float duration = 1f;
-
-        while (time < duration)
-        {
-            ovejaImage.rectTransform.position = Vector3.Lerp(firstShowSheepPos, showSheepPos, time / duration);
-            time += Time.deltaTime;
-            yield return null;
-        }
-
-        ovejaImage.rectTransform.position = showSheepPos;
-
-        yield return new WaitForSeconds(2f);
-
-        time = 0;
-        while (time < duration)
-        {
-            ovejaImage.rectTransform.position = Vector3.Lerp(showSheepPos, hideSheepPos, time / duration);
-            time += Time.deltaTime;
-            yield return null;
-        }
-
-        ovejaImage.rectTransform.position = hideSheepPos;
-    }
 }
